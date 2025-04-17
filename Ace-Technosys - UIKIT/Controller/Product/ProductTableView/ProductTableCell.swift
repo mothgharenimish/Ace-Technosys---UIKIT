64e0934eda814ff511144b9cb0be3d1e6c3ff09a
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-17 19:11:23 +0530

    Xcode Updated Changes

--- Ace-Technosys - UIKIT/Controller/Product/ProductTableView/ProductTableCell.swift
+++ Ace-Technosys - UIKIT/Controller/Product/ProductTableView/ProductTableCell.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ProductTableCell: UITableViewCell {
     
+    
+    //MARK: -IBOutlet 
     @IBOutlet weak var productView: UIView!
     @IBOutlet weak var productImg: UIImageView!
+    @IBOutlet weak var categoryLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var ratingLbl: UILabel!
+    @IBOutlet weak var ratingView: UIView!
+    @IBOutlet weak var statusView: UIView!
+    @IBOutlet weak var statusLbl: UILabel!
+    @IBOutlet weak var statusImg: UIImageView!
+    @IBOutlet weak var buynowView: UIView!
     
     
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
